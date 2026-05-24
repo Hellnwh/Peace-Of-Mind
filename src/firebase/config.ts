75e3a6c0a3837671799fcb90dac1c7bbc7725619
Hellnwh <irishiv2100@gmail.com>
2026-05-24 11:12:59 +0530

    Update firebaseConfig apiKey formatting

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3656866900-87e78",
   "appId": "1:1039659950137:web:1f0d7433a4d03887112de8",
-  "apiKey": "AIzaSyCWntzazVPiqqNmE0Q0qcuSP5YFS5rQJvE",
+  "apiKey":
   "authDomain": "studio-3656866900-87e78.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1039659950137"
