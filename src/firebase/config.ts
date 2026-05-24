ed858048834e0da76350af78452ccad5699e21ad
Hellnwh <irishiv2100@gmail.com>
2026-05-24 11:15:42 +0530

    Remove firebase configuration from config.ts
    Remove firebase configuration details from the file.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,1 @@
-export const firebaseConfig = {
-  "projectId": "studio-3656866900-87e78",
-  "appId": "1:1039659950137:web:1f0d7433a4d03887112de8",
-  "apiKey":
-  "authDomain": "studio-3656866900-87e78.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1039659950137"
-};
+
