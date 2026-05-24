7e1d0cf752f58f2d93ff2381d942eff7c150697f
Hellnwh <irishiv2100@gmail.com>
2026-05-24 11:38:34 +0530

    Add Firebase configuration to config.ts
    Added Firebase configuration details.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,1 +1,7 @@
-
+  "projectId": "studio-3656866900-87e78",
+  "appId": "1:1039659950137:web:1f0d7433a4d03887112de8",
+  "apiKey": "",
+  "authDomain": "studio-3656866900-87e78.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1039659950137"
+v
