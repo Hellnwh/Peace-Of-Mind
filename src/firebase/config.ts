467cbfd60a5326025149236943801589b81f622e
Hellnwh <irishiv2100@gmail.com>
2026-05-24 11:46:28 +0530

    Remove apiKey and measurementId values
    Removed values for apiKey and measurementId in Firebase config.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,6 @@
   "projectId": "studio-3656866900-87e78",
   "appId": "1:1039659950137:web:1f0d7433a4d03887112de8",
-  "apiKey": "",
+  "apiKey":,
   "authDomain": "studio-3656866900-87e78.firebaseapp.com",
-  "measurementId": "",
+  "measurementId":,
   "messagingSenderId": "1039659950137"
-v
